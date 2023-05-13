29509e36379c69f8f12426b0119df2886eb0adaf
KD-67 <kevin.a.dominey@gmail.com>
2023-05-13 19:53:15 +0300

    actually made the grid, last commit was stolen lol

--- javascript.js
+++ javascript.js
@@ -1,16 +1,18 @@
 const container = document.querySelector('#container');
 
 function makeGrid() {
-    for(let i = 0; i < 16; i++){
+    for(let i = 0; i < num; i++){
         const row = document.createElement('div');
         row.classList.add('row');
         container.append(row);
-        for (let j = 0; j < 16; j++){
+        for (let j = 0; j < num; j++){
             const column = document.createElement('div');
             column.classList.add('column');
             row.append(column);
         }
     }
 };
+
+let num = 16;
 
 makeGrid();
