e66452261dc589fc91b66d7a48dbce0269b03935
KD-67 <kevin.a.dominey@gmail.com>
2023-05-13 18:48:56 +0300

    16x16 grid

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+const container = document.querySelector('#container');
+
+function makeGrid() {
+    for(let i = 0; i < 16; i++){
+        const row = document.createElement('div');
+        row.classList.add('row');
+        container.append(row);
+        for (let j = 0; j < 16; j++){
+            const column = document.createElement('div');
+            column.classList.add('column');
+            row.append(column);
+        }
+    }
+};
+
+makeGrid();
